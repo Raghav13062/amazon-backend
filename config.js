ee4c703205ad90e7fa9e6fd05aafd64cdff60c59
Raghav Gurjar <raghav13062@gmail.com>
2024-10-03 15:19:43 +0530

    mongodb

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  port: process.env.PORT || 4000  // Default to 3002 if PORT is not set
+  port: process.env.PORT || 4000,
+  jwtSecret: process.env.JWT_SECRET,
 };
--- index.js
+++ index.js
@@ -9,30 +9,26 @@ const config = require('./config');
 
 const app = express();
 
-// Log the MongoDB URI to verify it's being loaded correctly
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
-// CORS Configuration
 const corsOptions = {
-  origin: '*', // Allow all origins
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api', cardRoutes);
 
-// Connect to MongoDB (removed deprecated options)
-mongoose.connect(config.mongoURI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: false,
+  useUnifiedTopology: false,
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Start server
 const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
