808e9a6daa2903b12ad688c2d7235c9b254aa50a
Raghav Gurjar <raghav13062@gmail.com>
2024-10-03 16:16:12 +0530

    mongodb

--- Ecosystem.config.js
+++ Ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
         script: 'index.js',
         watch: true,
         env: {
-          MONGO_URI: 'mongodb+srv://raghav13062:ddVkbskRfpuUJi5V@cluster0.1gkq4.mongodb.net/',
-          PORT: 4000,
-          JWT_SECRET: 'e218068f28279f44e96fce62249456e9b93493b5142e60712abfd6e036dc719474dc85f4a09dd6465faa4e8820a96859817dc7675fcb1c6157b4f88ad6dfbe2e',
+            mongoURI: process.env.MONGO_URI,
+            port: process.env.PORT || 4000,
+            jwtSecret: process.env.JWT_SECRET,
         },
       },
     ],
